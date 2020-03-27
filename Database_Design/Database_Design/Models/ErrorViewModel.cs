012227787e1ecb5037d87f288026b0672e206793
joe-Ru <ruizjoseph17@gmail.com>
2020-03-27 15:41:17 -0400

    Initial Commit

---
+++ Database_Design/Database_Design/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Database_Design.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
