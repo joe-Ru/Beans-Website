8b835f024a3f156e1ddae20baea53392093e7953
trey cox <tcollincox@gmaio.com>
2020-04-22 17:53:30 -0400

    Navigation features

---
+++ Database_Design/Database_Design/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Database_Design.Models;
+
+namespace Database_Design.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo) { repository = repo; }
+
+        public IViewComponentResult Invoke() { return View(repository.Products.Select(x => x.CategoryId).Distinct().OrderBy(x => x)); }
+    }
+}
