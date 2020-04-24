c906584ecc2d863ddc603cac254b515558a73065
tcollincox <38082239+tcollincox@users.noreply.github.com>
2020-04-24 11:06:56 -0400

    database for orders not loading. Checkout page

---
+++ Database_Design/Database_Design/Models/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Database_Design.Models
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> Orders { get; } void SaveOrder(Order order); 
+    }
+}
