a8e95c09e1e38db3ae23435984f3ad56ac63cef3
tcollincox <38082239+tcollincox@users.noreply.github.com>
2020-04-24 15:00:14 -0400

    fixed the check out

--- Database_Design/Database_Design/Models/EFOrderRepository.cs
+++ Database_Design/Database_Design/Models/EFOrderRepository.cs
@@ -2,17 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
-namespace Database_Design.Models { 
-    public class EFOrderRepository : IOrderRepository 
-    { private ApplicationDbContext context; 
-        public EFOrderRepository(ApplicationDbContext ctx) { context = ctx; } 
-        public IEnumerable<Order> Orders => context.Orders.Include(o => o.Lines).ThenInclude(l => l.Product); 
-        public void SaveOrder(Order order) { 
-            context.AttachRange(order.Lines.Select(l => l.Product)); 
-            if (order.OrderID == 0) { 
-                context.Orders.Add(order); 
-            }
-            context.SaveChanges(); 
-        } 
+namespace Database_Design.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext context;
+        public EFOrderRepository(ApplicationDbContext ctx) { context = ctx; }
+        public IEnumerable<Order> Orders => context.Orders.Include(o => o.Lines).ThenInclude(l => l.Product);
+        public void SaveOrder(Order order) { context.AttachRange(order.Lines.Select(l => l.Product)); if (order.OrderID == 0) { context.Orders.Add(order); } context.SaveChanges(); }
     }
 }
--- Database_Design/Database_Design/Startup.cs
+++ Database_Design/Database_Design/Startup.cs
@@ -37,6 +37,7 @@ namespace Database_Design
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc(options => options.EnableEndpointRouting = false).AddNewtonsoftJson(); services.AddMemoryCache(); services.AddSession();
             services.AddIdentity<User, IdentityRole>(opt =>
             {
