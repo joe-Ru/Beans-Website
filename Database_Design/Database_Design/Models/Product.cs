56c82b4bf0a0e9ea54fc5ba8195ed053660196b5
John Lowery <lowery.jack20@gmail.com>
2020-04-24 12:36:38 -0400

    John and Trey Branch Merge
    I did it

---
+++ Database_Design/Database_Design/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Database_Design.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductCreator { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsPreferred { get; set; }
+        public string CategoryId { get; set; }
+        
+    }
+}
---
+++ Database_Design/Database_Design/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Database_Design.Models;
+
+namespace Database_Design.Models.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
