b53b95d3a31d8d0b21989b6ca2911a22980a8335
joseph <ruizjoseph17@gmail.com>
2020-04-27 21:22:55 -0400

    Actual last commit from John

---
+++ Beans-Website/Database_Design/Database_Design/Models/UploadFileModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Database_Design.Models
+{
+    public class UploadFilePage
+    {
+        public IFormFile ImageUrl { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string CategoryId { get; set; }
+    }
+}
